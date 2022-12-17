464b9eb7e048bdc56f89e38610a46ccbd40d5678
mehmet <mehmetakkaya1990@gmail.com>
2022-12-17 17:39:52 +0300

    done

--- src/overloading/device/MyDevices.java
+++ src/overloading/device/MyDevices.java
@@ -4,6 +4,16 @@ public class MyDevices {
 
     public static void main(String[] args) {
 
+        TV tv = new TV("LG", "Ultimate",20000,false,true);
+        tv.channelDown();
+        System.out.println(tv);
+        Laptop laptop = new Laptop("Lenovo","IdeaPad",15000,true,true);
+        laptop.turnOn();
+        System.out.println(laptop);
+        Samsung samsung = new Samsung("s20",35000, true,true);
+        samsung.call(3124124124323l);
+        System.out.println(samsung);
+
     }
 
 }
